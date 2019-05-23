2f9679b9a17275dccd37aba54eb22602105199db
Nemanja Ćetković <cetko.ns@gmail.com>
2019-05-23 19:04:12 +0200

    frontend

---
+++ WebContent/precache-manifest.2ae807bcf33f9325e86717e7422afb98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c13094ae7254aa6b8692a702a43a002e",
+    "url": "/AgentTechnology/index.html"
+  },
+  {
+    "revision": "1e531c9b1a9b1012a97e",
+    "url": "/AgentTechnology/static/css/main.560b8c4b.chunk.css"
+  },
+  {
+    "revision": "a567a7a44ef73402e4a6",
+    "url": "/AgentTechnology/static/js/2.a3598054.chunk.js"
+  },
+  {
+    "revision": "1e531c9b1a9b1012a97e",
+    "url": "/AgentTechnology/static/js/main.e04dceaf.chunk.js"
+  },
+  {
+    "revision": "a00e7e5cfb849365c5d8",
+    "url": "/AgentTechnology/static/js/runtime~main.6040c9a3.js"
+  }
+]);
